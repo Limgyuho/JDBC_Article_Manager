3d2590c18ad9d60592957c14b9207f7bb7c2e99a
Limgyuho <radum0719@hanmail.net>
2023-03-13 15:47:12 +0900

    loginId 중복체크 기능 구현

--- src/com/KoreaIT/example/JAM/App.java
+++ src/com/KoreaIT/example/JAM/App.java
@@ -43,6 +43,21 @@ public class App {
 							System.out.println("아이디를 입력해주세요");
 							continue;
 						}
+						
+						SecSql sql = new SecSql();
+
+						sql.append("SELECT COUNT(*) > 0");
+						sql.append("FROM `member`");
+						sql.append("WHERE loginId = ?", loginId);
+
+						boolean isloginIdDup = DBUtil.selectRowBooleanValue(conn, sql);
+						
+						if(isloginIdDup){
+								System.out.printf("%s은(는) 이미 존재하는 아이디 입니다\n",loginId);
+								continue;
+						}
+						
+						
 						break;
 					}
 					
