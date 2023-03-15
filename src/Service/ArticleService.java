96753be9d3a368952fb5ddc6acec3db9b51a4025
Limgyuho <radum0719@hanmail.net>
2023-03-15 12:43:48 +0900

    MVC패턴으로 구조변경 진행중2

--- src/Service/ArticleService.java
+++ src/Service/ArticleService.java
@@ -1,10 +1,35 @@
 package Service;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.KoreaIT.example.JAM.Article;
+import com.KoreaIT.example.JAM.dao.ArticleDao;
+
 public class ArticleService {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private ArticleDao articleDao;
 
+	public ArticleService(Connection conn) {
+		this.articleDao = new ArticleDao(conn);
 	}
 
+	public int doWrite(String title, String body) {
+		return articleDao.doWrite(title, body);
+	}
+
+	public List<Article> getArticles() {
+
+		List<Map<String, Object>> articleListMap = articleDao.getArticles();
+
+		List<Article> articles = new ArrayList<>();
+
+		for (Map<String, Object> articleMap : articleListMap) {
+			articles.add(new Article(articleMap));
+		}
+
+		return articles;
+	}
 }
